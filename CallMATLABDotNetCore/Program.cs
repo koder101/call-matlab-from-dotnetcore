d8c78e392568f64eaabfcef58bbbe3700257e554
Zeeshaan Ali <zeeshaanali@hotmail.com>
2020-03-26 03:00:21 +0530

    Uploading the source code files.

---
+++ CallMATLABDotNetCore/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CallMATLABDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the MATLAB instance 
+            MLApp.MLApp matlab = new MLApp.MLApp();
+
+            // Change to the directory where the function is located 
+            matlab.Execute(@"cd G:\MatlabProject\FunctionCallFromDotNet");
+
+            // Define the output 
+            object result = null;
+
+            // Call the MATLAB function myfunc
+            matlab.Feval("cubicpoly", 1, out result, 2, 3, 1);
+
+            // Display result 
+            object[] res = result as object[];
+
+            Console.WriteLine(res[0]);
+            //Console.WriteLine(res[1]);
+            // Get user input to terminate program
+            Console.ReadLine();
+        }
+    }
+}
---
+++ cubicpoly.m
@@ -0,0 +1,4 @@
+function y = cubicpoly(a,b,c)
+y = a^3 + b^2 + c;
+return
+end
